6e945e5877042e4f1d7c71a351d85cc747400a1a
Vesa Poikajärvi <vesa.poikajarvi@leonidasoy.fi>
2012-08-15 09:28:45 +0300

    Do not emit error on handleError
    Due to reasons unknown to me the HTTP errors such as
    'getaddrinfo ENOENT' are returned to the caller in callback parameter
    if the parser.emit('error'); is removed.

--- main.js
+++ main.js
@@ -766,7 +766,6 @@ FeedParser.prototype.handleSaxError = function (e, scope){
 
 FeedParser.prototype.handleError = function (e, scope, next){
   var parser = scope;
-  parser.emit('error', e);
   parser.errors.push(e);
   if (typeof next === 'function') {
     next();
