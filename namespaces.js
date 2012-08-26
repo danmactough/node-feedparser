0c2a15aede26ddd64a9c64730827e873513a1b41
Dan MacTough <danmactough@gmail.com>
2012-08-26 00:27:16 -0400

    Add default namespaces lookup table.

---
+++ namespaces.js
@@ -0,0 +1,24 @@
+/*
+ * Default namespaces
+ *
+ * Lookup by URI
+ */
+
+module.exports = { 'http://www.w3.org/2005/Atom': 'atom' // v1.0
+                  , 'http://purl.org/atom/ns#': 'atom' // v0.3
+                  , 'http://www.w3.org/1999/02/22-rdf-syntax-ns#' : 'rdf'
+                  , 'http://purl.org/rss/1.0/' : 'rdf' // rss v1.0
+                  , 'http://purl.org/dc/elements/1.1/': 'dc'
+                  , 'http://www.itunes.com/dtds/podcast-1.0.dtd' : 'itunes'
+                  , 'http://search.yahoo.com/mrss/' : 'media'
+                  , 'http://rssnamespace.org/feedburner/ext/1.0' : 'feedburner'
+                  , 'http://www.pheedo.com/namespace/pheedo' : 'pheedo'
+                  , 'http://purl.org/rss/1.0/modules/content/' : 'content'
+                  , 'http://purl.oclc.org/net/rss_2.0/enc#' : 'enc'
+                  , 'http://webns.net/mvcb/' : 'admin'
+                  , 'http://purl.org/rss/1.0/modules/syndication/' : 'syn'
+                  , 'http://web.resource.org/cc/' : 'cc'
+                  , 'http://purl.org/rss/1.0/modules/taxonomy/' : 'taxo'
+                  , 'http://purl.org/syndication/thread/1.0': 'thr'
+                  , 'http://www.w3.org/1999/xhtml' : 'xhtml'
+                  };
