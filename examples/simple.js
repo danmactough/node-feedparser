b336b8e57421bfc401c51ab16ae3afd3d27daf23
Chris Anderson <jchris@couchbase.com>
2012-11-17 13:19:00 +0000

    make example work with new api
    The example was using deprecated methods.

--- examples/simple.js
+++ examples/simple.js
@@ -4,13 +4,9 @@
  * MIT Licensed
  */
 
-var FeedParser = require('../lib/feedparser')
-  , parser
-
-parser = new FeedParser();
+var feedparser = require(__dirname+'/..'),
+    parser = feedparser.parseUrl('http://cyber.law.harvard.edu/rss/examples/rss2sample.xml');
 
 parser.on('article', function(article){
     console.log('Got article: %s', JSON.stringify(article));
 });
-
-parser.parseFile('http://cyber.law.harvard.edu/rss/examples/rss2sample.xml');
