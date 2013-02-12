5b6fdf7104a29df3233ed77d02c08135e051d643
James Sharp <james@ortootech.com>
2013-02-12 09:03:06 +0000

    new dateformat option to allow feeds with odd dateformats to be parsed correctly

--- test/parseString-simple-options.js
+++ test/parseString-simple-options.js
@@ -42,5 +42,26 @@ describe('feedparser', function(){
       });
     });
   });
+  describe('parseString with custom date format', function() {
+    before(function(done){
+      var str = require('fs').readFileSync(__dirname + '/feeds/rss2sample-odddatetime.xml');
+      FeedParser.parseString(str, {dateformat: 'DD/MM/YYYY HH:mm:ss'}, function (err, _meta, _articles){
+        assert.ifError(err);
+        meta = _meta;
+        articles = _articles;
+        done();
+      });
+    });
+    describe('meta', function(){
+      it('should have a last build date of feb 9th 2013', function() {
+        assert.equal(new Date('Feb 9 2013').getTime(), meta.date.getTime());
+      });
+    });
+    describe('article 1', function(){
+      it('should have a pubdate time of feb 8 2013, 12:00', function(){
+        assert.equal(new Date('Feb 8 2013, 12:00:00').getTime(), articles[0].pubdate.getTime());
+      });
+    });
+  });
 });
 
