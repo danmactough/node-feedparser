42feb773248caffebf3de4ac9712b58f55cf1d22
Dan MacTough <danmactough@gmail.com>
2013-03-25 00:04:37 -0400

    Add basic test for writable stream input api

---
+++ test/api-stream.js
@@ -0,0 +1,42 @@
+describe('Writeable Stream Input API', function () {
+
+  var feedPath = __dirname + '/feeds/rss2sample.xml';
+
+  var events = [];
+
+  beforeEach(function () {
+    events = [];
+  })
+
+  afterEach(function () {
+    assert.equal(events.indexOf('error'), -1);
+    assert.ok(~events.indexOf('meta'));
+    assert.ok(~events.indexOf('article'));
+    assert.ok(~events.indexOf('complete'));
+  });
+
+  describe('.pipe()', function () {
+    it('works', function (done) {
+      require('fs').createReadStream(feedPath).pipe(new FeedParser())
+        .on('error', function (err) {
+          assert.ifError(err);
+          events.push('error');
+        })
+        .on('meta', function (meta) {
+          assert.notEqual(meta, null);
+          events.push('meta');
+        })
+        .on('article', function (article) {
+          assert.notEqual(article, null);
+          events.push('article');
+        })
+        .on('complete', function (meta, articles) {
+          assert.notEqual(meta, null);
+          assert.ok(articles.length);
+          events.push('complete');
+        })
+        .on('end', done);
+    });
+  });
+
+});
