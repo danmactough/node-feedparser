e229eaec4b2b06fdb3344c5590fcabac9e8bc95b
Dan MacTough <danmactough@gmail.com>
2013-05-29 23:03:39 -0400

    Make tests run on v0.10. Closes #61.

--- test/common.js
+++ test/common.js
@@ -2,23 +2,37 @@
 var URL = require('url');
 
 assert = require('assert');
+var fs = require('fs')
+  , path = require('path')
+  , zlib = require('zlib')
+  , gzip = zlib.createGzip();
+
 FeedParser = require('../');
 server = function (done) {
   var app = require('http').createServer();
-  var buffet = require('buffet')({ root: __dirname + '/feeds', indexes: false, watch: false });
   app.on('request', function (req, res) {
-    var url = URL.parse(req.url, true);
-    // gzip the response unless the Accept-Encoding header says otherwise - issue #36
-    if (url.query['gzip'] === 'true') {
-      req.headers['accept-encoding'] = req.headers['accept-encoding'] || 'deflate, gzip';
-    }
+    var url = URL.parse(req.url, true)
+      , file = path.resolve(__dirname, 'feeds', url.pathname.replace(/^\//, ''));
     if (url.query['notModified'] === 'true') {
       res.writeHead('304', 'Not modified');
       res.end();
     }
+    else {
+      fs.exists(file, function (exists) {
+        if (!exists) {
+          res.writeHead('404', 'Not found');
+          return res.end();
+        }
+        // gzip the response unless the Accept-Encoding header says otherwise - issue #36
+        if (url.query['gzip'] === 'true' && !req.headers['accept-encoding'].match(/identity/i)) {
+          fs.createReadStream(file).pipe(gzip).pipe(res);
+        }
+        else {
+          fs.createReadStream(file).pipe(res);
+        }
+      });
+    }
   });
-  app.on('request', buffet);
-  app.on('request', buffet.notFound);
   app.listen(21337, function () {
     done && done();
   });
@@ -26,7 +40,6 @@ server = function (done) {
   server.close = function (done) {
     app.close.call(app, function (){
       delete server.app;
-      delete require.cache.buffet;
       done && done();
     });
   };
