c9eea2d49ba61e9be34cdfe5d153b2b2a8298dda
Dan MacTough <danmactough@gmail.com>
2013-06-03 23:52:05 -0400

    Fix processing instruction handler to avoid interpretting extraneouso whitespace as attribute names.

--- main.js
+++ main.js
@@ -151,7 +151,7 @@ FeedParser.prototype.handleError = function (next, e){
 
 FeedParser.prototype.handleProcessingInstruction = function (node) {
   if (node.name !== 'xml') return;
-  this.meta['#xml'] = node.body.split(' ').reduce(function (map, attr) {
+  this.meta['#xml'] = node.body.trim().split(' ').reduce(function (map, attr) {
     var parts = attr.split('=');
     map[parts[0]] = parts[1] && parts[1].length > 2 && parts[1].match(/^.(.*?).$/)[1];
     return map;
