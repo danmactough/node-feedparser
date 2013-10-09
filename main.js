89066e7f5913d36912bdf7f50636e37a2f27ebd0
Paul Mougel <paul.mougel@atos.net>
2013-10-09 14:52:21 +0200

    Revert "Reduced memory consumption."
    This reverts commit bd74a78f0ec0b1707e74424f26f010614ee89302.

--- main.js
+++ main.js
@@ -98,6 +98,7 @@ FeedParser.prototype.init = function (){
   };
   this._emitted_meta = false;
   this.stack = [];
+  this.nodes = {};
   this.xmlbase = [];
   this.in_xhtml = false;
   this.xhtml = {}; /* Where to store xhtml elements as associative
@@ -335,6 +336,8 @@ FeedParser.prototype.handleCloseTag = function (el){
     } else {
       this.stack[0][stdEl] = [this.stack[0][stdEl], n];
     }
+  } else {
+    this.nodes = n;
   }
 };
 
