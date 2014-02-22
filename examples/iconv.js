c397f59ddb08ad01776913f2ef34ad3a0582666b
William P. Riley-Land <william@kun.io>
2014-02-22 15:54:22 -0600

    Fix small typo.

--- examples/iconv.js
+++ examples/iconv.js
@@ -13,7 +13,7 @@ function fetch(feed) {
   // Define our streams
   var req = request(feed, {timeout: 10000, pool: false});
   req.setMaxListeners(50);
-  // Some feeds do not response without user-agent and accept headers.
+  // Some feeds do not respond without user-agent and accept headers.
   req.setHeader('user-agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36')
      .setHeader('accept', 'text/html,application/xhtml+xml');
 
