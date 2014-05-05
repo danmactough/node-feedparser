df2870d9193262584dac1e8ae03ddaef8b6a6720
Dan MacTough <danmactough@gmail.com>
2014-05-05 18:03:06 -0400

    Remove unnecessary test server

--- test/common.js
+++ test/common.js
@@ -1,46 +1,4 @@
-/*global assert:true, FeedParser:true, server:true*/
-var URL = require('url');
-
+/*global assert:true, FeedParser:true, fs:true*/
 assert = require('assert');
-var path = require('path')
-  , zlib = require('zlib')
-  , gzip = zlib.createGzip();
-
 fs = require('fs');
 FeedParser = require('../');
-server = function (done) {
-  var app = require('http').createServer();
-  app.on('request', function (req, res) {
-    var url = URL.parse(req.url, true)
-      , file = path.resolve(__dirname, 'feeds', url.pathname.replace(/^\//, ''));
-    if (url.query['notModified'] === 'true') {
-      res.writeHead('304', 'Not modified');
-      res.end();
-    }
-    else {
-      fs.exists(file, function (exists) {
-        if (!exists) {
-          res.writeHead('404', 'Not found');
-          return res.end();
-        }
-        // gzip the response unless the Accept-Encoding header says otherwise - issue #36
-        if (url.query['gzip'] === 'true' && !req.headers['accept-encoding'].match(/identity/i)) {
-          fs.createReadStream(file).pipe(gzip).pipe(res);
-        }
-        else {
-          fs.createReadStream(file).pipe(res);
-        }
-      });
-    }
-  });
-  app.listen(21337, function () {
-    done && done();
-  });
-  server.app = app;
-  server.close = function (done) {
-    app.close.call(app, function (){
-      delete server.app;
-      done && done();
-    });
-  };
-};
