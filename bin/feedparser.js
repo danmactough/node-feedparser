786cd0a3a7fe2d8a881f2dc74d87416391f2eaa0
Dan MacTough <danmactough@gmail.com>
2016-12-26 00:05:20 -0500

    Make bin script useful as command line tool and rename to "feedparser"
    Useful part mainly being that it will output JSON if output is not a tty (i.e., when piping)

--- bin/feedparser.js
+++ bin/feedparser.js
@@ -2,19 +2,26 @@
 /*
  * Parse a feed and dump the result to the console
  *
- * Usage: curl <feed url> | bin/dump.js
- *        cat <feed file> | bin/dump.js
+ * Usage: curl <feed url> | bin/feedparser.js
+ *        cat <feed file> | bin/feedparser.js
  *
  */
 var isatty = require('tty').isatty
   , util = require('util')
   , FeedParser = require('../');
 
+var usingConsole = isatty(1) && isatty(2);
+
 process.stdin.pipe(new FeedParser())
   .on('error', console.error)
   .on('readable', function() {
     var stream = this, item;
     while (item = stream.read()) {
-      console.log(util.inspect(item, null, 10, isatty(1) && isatty(2)));
+      if (usingConsole) {
+        console.log(util.inspect(item, null, 10, true));
+      }
+      else {
+        console.log(JSON.stringify(item));
+      }
     }
   });
