515dc74124824f4b39526ead7fcdb112eab8ae2f
Adriana Saroz <adriana@watchup.com>
2017-01-18 16:16:43 +0200

    Keep optional media:content attributes in the enclosures default property

--- test/duplicate-enclosures.js
+++ test/duplicate-enclosures.js
@@ -6,7 +6,26 @@ describe('duplicate enclosures', function(){
     fs.createReadStream(feed).pipe(new FeedParser())
       .once('readable', function () {
         var stream = this;
-        assert.strictEqual(stream.read().enclosures.length, 2);
+        var enclosures = stream.read().enclosures;
+        assert.strictEqual(enclosures.length, 3);
+        assert.deepEqual(enclosures, [{
+          url: 'http://i.mol.im/i/pix/2013/02/03/article-2272640-174FCEE2000005DC-697_154x115.jpg',
+          type: 'image/jpeg',
+          length: '4114',
+          height: '115',
+          width: '154'
+        }, {
+          url: 'http://i.mol.im/i/pix/2013/02/03/article-2272640-174FCEE2000005DC-697_154x115.mp4',
+          type: 'video/mp4',
+          length: '4114'
+        }, {
+          url: 'http://i.mol.im/i/pix/2013/02/03/article-2272640-174FCEE2000005DC-697.mp4',
+          type: 'video/mp4',
+          length: null,
+          bitrate: '3000',
+          height: '115',
+          width: '154'
+        }]);
         done();
       })
       .on('error', function (err) {
