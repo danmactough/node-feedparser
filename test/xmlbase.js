f43b4458feb1d6eed54b94711e78abc78dff8ac8
Dan MacTough <danmactough@gmail.com>
2017-12-09 15:54:24 -0500

    Fix reresolve helper to correctly resolve relative URLs in RSS channel image
    Relative URLs are not acceptable here, but they happen.
    
    Resolves #152

--- test/xmlbase.js
+++ test/xmlbase.js
@@ -15,6 +15,19 @@ describe('xmlbase', function(){
       });
   });
 
+  it('should resolve relative image url in channel', function (done) {
+    var feed = __dirname + '/feeds/relative-channel-image-url.xml';
+    fs.createReadStream(feed).pipe(new FeedParser())
+      .on('meta', function (meta) {
+        assert.equal('https://www.virtualbox.org/graphics/vbox_logo2_gradient.png', meta.image.url);
+        done();
+      })
+      .on('error', function (err) {
+        assert.ifError(err);
+        done(err);
+      });
+  });
+
   it('should parse feedurl option and handle relative URIs with no root xml:base', function (done) {
     var feed = __dirname + '/feeds/intertwingly.atom';
     var options = { feedurl: 'http://intertwingly.net/blog/index.atom' };
